3ed67e9b2115ac17b6b310421028a3e600ab1b45
Adeel Ali <adeel.asd.aa@gmail.com>
2021-03-03 20:02:44 +0500

    ts

---
+++ vite.config.ts
@@ -0,0 +1,58 @@
+// vite.config.js
+import path from "path";
+import svelte from "@svitejs/vite-plugin-svelte";
+import windiCSS from "vite-plugin-windicss";
+import { minify } from "html-minifier";
+import preprocess from "svelte-preprocess"
+
+// const { defineConfig } = require("vite");
+import { defineConfig } from "vite"
+const indexReplace = () => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return minify(html, {
+        collapseWhitespace: true,
+      });
+    },
+  };
+};
+
+export default defineConfig(({ command, mode }) => {
+
+  const isProduction = mode === "production";
+  return {
+    extensions: ['html', 'js', 'css', 'svg', 'json', 'ts'],
+    optimizeDeps: {
+      exclude: ["@roxi/routify"],
+    },
+    resolve: {
+      alias: {
+        svelte: path.resolve(__dirname, "node_modules/svelte"),
+      },
+    },
+    plugins: [
+      windiCSS({
+        //@ts-ignore
+        verbose: true,
+        silent: false,
+        debug: true,
+        config: "tailwind.config.js", // tailwind config file path (optional)
+        compile: false, // false: interpretation mode; true: compilation mode
+        prefix: "windi-", // set compilation mode style prefix
+        globalPreflight: true, // set preflight style is global or scoped
+        globalUtility: true, // set utility style is global or scoped
+      }),
+      svelte({
+        //@ts-ignore
+        hot: !isProduction,
+        emitCss: true,
+        preprocess: preprocess()
+      }),
+      indexReplace(),
+    ],
+    build: {
+      minify: isProduction,
+    },
+  };
+});
